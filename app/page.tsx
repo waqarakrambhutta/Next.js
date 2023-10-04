5a371fc731bfd09fc1bd8829d3efb6934bfeacef
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-10-05 02:45:27 +0500

    Routing and Navigation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main><h1>Hello World</h1></main>
+    <main>
+      <h1>Hello World</h1>
+      <Link href="/users">Users</Link> // to link pages in next we use link instead of anchor tag (anchor tag cause reload of whole app but link doesn't do this)
+      </main>
   )
 }
