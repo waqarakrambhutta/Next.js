2ccd30ddb2c4a71d7b77878d7b98493bf2f0eae6
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-10-07 23:55:03 +0500

    Client VS server rendering

---
+++ app/components/ProductCart.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddtoCart from "./AddtoCart";
+
+const ProductCart = () => {
+  return (
+    <div>
+      <AddtoCart></AddtoCart>
+    </div>
+  );
+};
+
+export default ProductCart;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import ProductCart from './components/ProductCart'
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
-      <Link href="/users">Users</Link> // to link pages in next we use link instead of anchor tag (anchor tag cause reload of whole app but link doesn't do this)
+      <Link href="/users">Users</Link> {/*to link pages in next we use link instead of anchor tag (anchor tag cause reload of whole app but link doesn't do this)*/} 
+      <ProductCart></ProductCart>
       </main>
       
   )
