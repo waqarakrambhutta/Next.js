3e9dc268e82e7f15db2f837332cb546ba822f5e7
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-10-07 17:57:32 +0500

    for streek

--- app/page.tsx
+++ app/page.tsx
@@ -7,5 +7,6 @@ export default function Home() {
       <h1>Hello World</h1>
       <Link href="/users">Users</Link> // to link pages in next we use link instead of anchor tag (anchor tag cause reload of whole app but link doesn't do this)
       </main>
+      
   )
 }
