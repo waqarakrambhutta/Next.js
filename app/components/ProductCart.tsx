0e4326dfbe34f4a3d0d3cb4080246759f36622a7
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-10-09 12:37:51 +0500

    Styling by tailwind.css

--- app/components/ProductCart.tsx
+++ app/components/ProductCart.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import AddtoCart from "./AddtoCart";
-import styles from './ProductCart.module.css'
 
 const ProductCart = () => {
   return (
-    <div className={styles.card}>
+    <div className={'p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'}>
       <AddtoCart/>
     </div>
   );
