e19ca064c8e3bb42a35402cb884e80a998acc79b
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-10-11 00:36:52 +0500

    Using daisy UI

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,24 +3,33 @@ import React from "react";
 interface User {
   id: number;
   name: string;
+  email: string;
 }
 
 const UsersPage = async () => {
-  const res = await fetch(
-    "https://jsonplaceholder.typicode.com/users",
-    { cache: 'no-store'}
-  );
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
   const users: User[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
       <p>{new Date().toLocaleTimeString()}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <table className="table">
+        <thead>
+          <th>Name</th>
+          <th>Email</th>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
