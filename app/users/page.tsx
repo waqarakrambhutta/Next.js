c8f057f8193fe94295321b90d96dc4884367f6cb
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-10-11 20:18:02 +0500

    Routing and Navigation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,35 +1,14 @@
 import React from "react";
+import UserTable from "./UserTable";
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
 
-const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: "no-store",
-  });
-  const users: User[] = await res.json();
+
+const UsersPage = () => {
 
   return (
     <>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <table className="table">
-        <thead>
-          <th>Name</th>
-          <th>Email</th>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable/>
     </>
   );
 };
