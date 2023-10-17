5226eeb700495359a48c0aa8f555de2173242bf4
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-10-17 19:20:05 +0500

    One way for showing Loading UIs in the pages

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import UserTable from "./UserTable";
 import { sort } from "fast-sort";
 import Link from "next/link";
@@ -12,8 +12,9 @@ const UsersPage = ({searchParams:sortOrder}:Props) => {
     <>
       <h1>Users</h1>
       <Link href={'/users/new'} className="btn">New User</Link>
+      <Suspense fallback={<p>Loading...</p>}>
       <UserTable searchParams={sortOrder} />
-      
+      </Suspense>
     </>
   );
 };
