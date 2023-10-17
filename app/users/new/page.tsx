5cc0161ac368a60c2c202e45ac0d5afaa1cdfc2a
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-10-17 19:05:39 +0500

    Programatic Navigation.

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,8 +1,17 @@
+'use client'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const page = () => {
+  const router = useRouter();
+
   return (
-    <div>New page</div>
+    <div>
+      <button 
+      className='btn btn-primary'
+      onClick={()=>router.push('/users')}
+      >Create</button>
+    </div>
   )
 }
 
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UserTable from "./UserTable";
 import { sort } from "fast-sort";
+import Link from "next/link";
 
 interface Props{
   searchParams:{sortOrder:string}
@@ -10,6 +11,7 @@ const UsersPage = ({searchParams:sortOrder}:Props) => {
   return (
     <>
       <h1>Users</h1>
+      <Link href={'/users/new'} className="btn">New User</Link>
       <UserTable searchParams={sortOrder} />
       
     </>
